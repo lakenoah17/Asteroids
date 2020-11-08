0a5873e54b244e2543e879829938929a79763a27
Noah Shields <noahshields5@gmail.com>
2020-11-07 20:59:15 -0500

    Implemented constuctors for a basic GameObject. Next step is to change the renderer to be easily utilized by a GameObject

---
+++ src/game_logic/GameObject.cpp
@@ -0,0 +1,70 @@
+#include "GameObject.h"
+
+GameObject::GameObject()
+{
+	position = new glm::vec2(5, 5);
+	collider = new Collider(0, 0, 10, 10);
+
+	verticies = new float[16]
+	{
+		0.0f, 0.0f, 0.0f, 0.0f,
+		5.0f, 0.0f, 1.0f, 0.0f,
+		5.0f, 5.0f, 1.0f, 1.0f,
+		0.0f, 5.0f, 0.0f, 1.0f
+	};
+
+	indicies = new int[6]{
+		2, 1, 0,
+		0, 3, 2
+	};
+}
+
+GameObject::GameObject(Collider* collider)
+	: collider(collider)
+{
+	position = new glm::vec2(collider->GetXPos() + collider->GetWidth() / 2, collider->GetYPos() + collider->GetHeight() / 2);
+
+	verticies = new float[16]
+	{
+		collider->GetXPos(),						collider->GetYPos(),						 0.0f, 0.0f,
+		collider->GetXPos() + collider->GetWidth(), collider->GetYPos(),					     1.0f, 0.0f,
+		collider->GetXPos() + collider->GetWidth(), collider->GetYPos() + collider->GetHeight(), 1.0f, 0.0f,
+		collider->GetXPos(),					    collider->GetYPos() + collider->GetHeight(), 1.0f, 0.0f
+	};
+
+	indicies = new int[6]{
+		2, 1, 0,
+		0, 3, 2
+	};
+}
+
+GameObject::GameObject(float x, float y, float width, float height)
+{
+	collider = new Collider(x, y, width, height);
+	position = new glm::vec2(x + width/2, y + height/2);
+
+	verticies = new float[16]
+	{
+		x,		   y,		   0.0f, 0.0f,
+		x + width, y,          1.0f, 0.0f,
+		x + width, y + height, 1.0f, 0.0f,
+		x,         y + height, 1.0f, 0.0f
+	};
+
+	indicies = new int[6]{
+		2, 1, 0,
+		0, 3, 2
+	};
+}
+
+GameObject::~GameObject()
+{
+	delete collider;
+	delete position;
+	delete verticies;
+	delete indicies;
+}
+
+void GameObject::Draw()
+{
+}
--- src/game_logic/GameObject.h
+++ src/game_logic/GameObject.h
@@ -5,14 +5,18 @@ class GameObject
 {
 private:
 	Collider* collider;
-	float xPos;
-	float yPos;
+	//Position is at the center of the rectangle
+	glm::vec2*  position;
+	
+	float* verticies;
+	int* indicies;
 
 public:
 	GameObject();
+	GameObject(Collider* collider);
 	GameObject(float x, float y, float width, float height);
 
 	~GameObject();
 
-
+	void Draw();
 };
